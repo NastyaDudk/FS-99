0ab1370078dec91af99d25510b0f21b0a5096a8f
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2023-11-22 15:45:30 +0400

    newchanged

--- java/window.js
+++ java/window.js
@@ -25,3 +25,42 @@ const openModal = () => {
 
 buttonElem.addEventListener('click', openModal);
 modalElem.addEventListener('click', closeModal);
+
+
+
+
+
+
+
+const button = document.querySelector('.loved-btn');
+const modal = document.querySelector('.backdrop');
+
+modal.style.cssText = `
+  display: flex;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 300ms ease-in-out;
+`;
+
+const close = event => {
+  const target = event.target;
+
+  let span = document.getElementsByClassName("modal-btn")[0];
+
+  setTimeout(() => {
+    modal.style.visibility = 'hidden';
+  }, 300);
+}
+
+const open = () => {
+  modal.style.visibility = 'visible';
+  modal.style.opacity = 1;
+}
+
+button.addEventListener('click', open);
+modal.addEventListener('click', close);
+
+
+
+
+
