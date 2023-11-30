d005d48bcc6ab17f76600e5b9d2feb851099c858
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2023-11-30 14:38:03 +0400

    modaldone

--- java/window.js
+++ java/window.js
@@ -1,5 +1,5 @@
-const buttonElem = document.querySelector('.menu-open-btn');
-const modalElem = document.querySelector('.mob-menu');
+const buttonElem = document.querySelector(".menu-open-btn");
+const modalElem = document.querySelector(".mob-menu");
 
 modalElem.style.cssText = `
   display: flex;
@@ -8,82 +8,61 @@ modalElem.style.cssText = `
   transition: opacity 300ms ease-in-out;
 `;
 
-const closeModal = event => {
+const closeModal = (event) => {
   const target = event.target;
 
   let span = document.getElementsByClassName("menu-close-btn")[0];
 
   setTimeout(() => {
-    modalElem.style.visibility = 'hidden';
+    modalElem.style.visibility = "hidden";
   }, 300);
-}
+};
 
 const openModal = () => {
-  modalElem.style.visibility = 'visible';
+  modalElem.style.visibility = "visible";
   modalElem.style.opacity = 1;
-}
+};
 
-buttonElem.addEventListener('click', openModal);
-modalElem.addEventListener('click', closeModal);
+buttonElem.addEventListener("click", openModal);
+modalElem.addEventListener("click", closeModal);
 
+// const button = document.querySelector('.loved-btn');
+// const modal = document.querySelector('.backdrop');
 
+// modal.style.cssText = `
+//   display: flex;
+//   visibility: hidden;
+//   opacity: 0;
+//   transition: opacity 300ms ease-in-out;
+// `;
 
+// const close = event => {
+//   const target = event.target;
 
+//   if(target === modal || target.closest('.modal-btn')) {
 
-const button = document.querySelector('.loved-btn');
-const modal = document.querySelector('.backdrop');
+//     modal.style.opacity = 0;
+//   }
 
-modal.style.cssText = `
-  display: flex;
-  visibility: hidden;
-  opacity: 0;
-  transition: opacity 300ms ease-in-out;
-`;
+//   setTimeout(() => {
+//     modal.style.visibility = 'hidden';
+//   }, 300);
+// }
 
-const close = event => {
-  const target = event.target;
+// const open = () => {
+//   modal.style.visibility = 'visible';
+//   modal.style.opacity = 1;
+// }
 
-  if(target === modal || target.closest('.modal-btn')) {
+// button.addEventListener('click', open);
+// modal.addEventListener('click', close);
 
-    modal.style.opacity = 0;
-  }
+$(function () {
+  $(".open").click(function () {
+    $(".backdrop").addClass("is-open");
+  });
 
-  setTimeout(() => {
-    modal.style.visibility = 'hidden';
-  }, 300);
-}
-
-const open = () => {
-  modal.style.visibility = 'visible';
-  modal.style.opacity = 1;
-}
-
-button.addEventListener('click', open);
-modal.addEventListener('click', close);
-
-
-// $(function () {
-
-//   $('.open').click(function(){
-//     if ($(".modal").hasClass("is open")) {
-//       ('.modal').removeClass('is open');
-//     }
-//       else
-//       {
-//         $(".modal").addClass("is open");
-//       }
-//     });
-  
-//     $('.close').click(function(){
-//       $('.modal').removeClass('is open');
-//     });
-// });
-
-
-
-
-
-
-
-
-
+  $("#close-btn").click(function () {
+    $(".backdrop").removeClass("is-open");
+  });
+});
